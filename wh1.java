0b30e56eb3282acd8b3d7e1ffe1e31bc3186f901
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2020-12-27 01:12:02 +0530

    fun program2
    This is a fun program and in this program we are printing "Hii, There!!" atleast one time at the time of condition false using do-while statement.

---
+++ wh1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class wh1
+{
+public static void main(String[] args)
+{
+int i=1;
+do
+{
+System.out.println("Hii, There!!");
+i++;
+}
+while(i<=0);
+
+}
+}
