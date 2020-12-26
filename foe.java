052edb92d023c6b273f9c55a38eb3e89014303fb
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2020-12-27 01:32:35 +0530

    Enhanced for loop program
    This is a Enhanced for loop program, In this program we are printing the array number using Enhanced for loop and one more thing if the value is equal to 5 means that program will stop the printing the array values.

---
+++ foe.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class foe
+{
+public static void main(String[] args)
+{
+int a[]={1,2,3,4,5,6,7,8,9};
+int e;
+System.out.println("numbers");
+for(int b:a)
+{
+
+System.out.println(b);
+if(b==5)
+break;
+}
+}
+}
