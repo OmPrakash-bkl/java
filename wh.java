363a5a74f8f84a04db46270dbbda885440e78485
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2020-12-27 01:07:48 +0530

    fun program
    This is a simple and funny program using while loop. In this program we are printing the "Hii, There!!" five times.

---
+++ wh.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class wh
+{
+public static void main(String[] args)
+{
+int i=1;
+while(i<=5)
+{
+System.out.println("Hii, There!!");
+i++;
+}
+
+}
+}
